0c9d4fe86c91cd6c8c46fe3400891942669fed2e
Sebas Ørøzcø <Sebas rzc>
2016-10-22 10:26:12 -0500

    Initial commit

---
+++ app/src/main/java/com/uniquindo/edu/co/proyecto/MainActivity.java
@@ -0,0 +1,13 @@
+package com.uniquindo.edu.co.proyecto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
